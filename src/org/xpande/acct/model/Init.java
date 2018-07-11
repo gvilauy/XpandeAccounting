0ff584efc83b79cc70366609121c654391b8f5ba
gvilauy <gabrielvila13@gmail.com>
2018-07-11 11:42:57 -0300

    OK

--- src/org/xpande/acct/model/Init.java
+++ src/org/xpande/acct/model/Init.java
@@ -1,8 +1,8 @@
-package org.xpande.model.acct;
+package org.xpande.acct.model;
 
 /**
  * Product: Adempiere ERP & CRM Smart Business Solution. Localization : Uruguay - Xpande
- * Xpande. Created by Gabriel Vila on 7/23/17.
+ * Xpande. Created by Gabriel Vila on 7/11/18.
  */
 public class Init {
 }
