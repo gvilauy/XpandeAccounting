6b2efe66dd6df0b06fefda6e4734342b2e174a5d
gabriel <gabrielvila13@gmail.com>
2018-12-10 15:00:37 -0300

    OK
    Signed-off-by: gabriel <gabrielvila13@gmail.com>

--- src/org/xpande/acct/model/MZGeneraFormDGI.java
+++ src/org/xpande/acct/model/MZGeneraFormDGI.java
@@ -437,14 +437,119 @@ public class MZGeneraFormDGI extends X_Z_GeneraFormDGI {
         ResultSet rs = null;
 
         try{
-            sql = "";
+
+            MAcctSchema as = (MAcctSchema) this.getC_AcctSchema();
+
+            sql = " select a.z_sistecopazostaxtkrut_id as c_invoice_id, sdgi.c_doctype_id as c_doctypetarget_id, " +
+                    " (coalesce(cfe.st_seriecfe,'') || cfe.st_numerocfe) as documentnoref, a.datetrx as dateinvoiced, " +
+                    " a.datetrx as dateacct, bp.c_bpartner_id, 142::numeric(10,0) as c_currency_id, tax.c_tax_id, " +
+                    " a.taxamt, 'Y'::character varying(1) as issotrx, " +
+                    " bp.c_taxgroup_id, coalesce(bp.taxid, a.st_documentoreceptor) as taxid , bp.value, bp.name, vc.account_id, vcVta.account_id as account_vta_id " +
+                    " from z_sistecopazostaxtkrut a " +
+                    " left outer join c_bpartner bp on a.st_documentoreceptor = bp.taxid " +
+                    " left outer join z_sisteco_tk_cfecab cfe on a.z_sisteco_tk_cvta_id = cfe.z_sisteco_tk_cvta_id " +
+                    " left outer join z_cfe_configdocdgi dgi on cfe.st_tipocfe = dgi.codigodgi " +
+                    " left outer join z_cfe_configdocsend sdgi on dgi.z_cfe_configdocdgi_id = sdgi.z_cfe_configdocdgi_id " +
+                    " left outer join c_tax tax on (a.c_taxcategory_id = tax.c_taxcategory_id and tax.isdefault='Y') " +
+                    " left outer join c_tax_acct tacct on tax.c_tax_id = tacct.c_tax_id " +
+                    " left outer join c_validcombination vc on tacct.t_credit_acct = vc.c_validcombination_id " +
+                    " left outer join c_validcombination vcVta on tacct.t_due_acct = vcVta.c_validcombination_id " +
+                    " where a.ad_org_id =" + this.getAD_Org_ID() +
+                    " and a.datetrx between ? and ? " +
+                    " and a.taxamt != 0 " +
+                    " order by a.c_taxcategory_id, a.datetrx ";
 
         	pstmt = DB.prepareStatement(sql, get_TrxName());
+            pstmt.setTimestamp(1, this.getStartDate());
+            pstmt.setTimestamp(2, this.getEndDate());
+
         	rs = pstmt.executeQuery();
 
         	while(rs.next()){
 
-        	}
+                boolean isSOTrx = (rs.getString("issotrx").equalsIgnoreCase("Y")) ? true : false;
+
+                String nroIdentificacion = rs.getString("taxid");
+
+                if ((nroIdentificacion == null) || (nroIdentificacion.trim().equalsIgnoreCase(""))){
+
+                    MZGeneraFormDGIError dgiError = new MZGeneraFormDGIError(getCtx(), 0, get_TrxName());
+                    dgiError.setZ_GeneraFormDGI_ID(this.get_ID());
+                    dgiError.setErrorMsg("Socio de Negocio NO tiene NUMERO DE IDENTIFICACIÓN : " + rs.getString("value") +
+                            " - " + rs.getString("name"));
+                    dgiError.saveEx();
+                }
+                else{
+
+                    if (rs.getInt("c_bpartner_id") <= 0){
+
+                        MZGeneraFormDGIError dgiError = new MZGeneraFormDGIError(getCtx(), 0, get_TrxName());
+                        dgiError.setZ_GeneraFormDGI_ID(this.get_ID());
+                        dgiError.setErrorMsg("No existe un Socio de Negocio definido con NUMERO DE IDENTIFICACIÓN : " + nroIdentificacion);
+                        dgiError.saveEx();
+                    }
+                    else{
+                        MZGeneraFormDGILin linea = new MZGeneraFormDGILin(getCtx(), 0, get_TrxName());
+                        linea.setZ_GeneraFormDGI_ID(this.get_ID());
+                        linea.setAmtDocument(rs.getBigDecimal("taxamt"));
+                        linea.setAmtDocumentMT(linea.getAmtDocument());
+                        linea.setC_BPartner_ID(rs.getInt("c_bpartner_id"));
+                        linea.setC_Currency_ID(rs.getInt("c_currency_id"));
+                        linea.setC_DocType_ID(rs.getInt("c_doctypetarget_id"));
+                        linea.setC_Invoice_ID(rs.getInt("c_invoice_id"));
+                        linea.setC_Tax_ID(rs.getInt("c_tax_id"));
+                        linea.setCurrencyRate(Env.ONE);
+                        linea.setDateAcct(rs.getTimestamp("dateacct"));
+                        linea.setDateDoc(rs.getTimestamp("dateinvoiced"));
+                        linea.setDocumentNoRef(rs.getString("documentnoref"));
+                        linea.setTaxID(nroIdentificacion);
+                        linea.setC_TaxGroup_ID(rs.getInt("c_taxgroup_id"));
+
+                        int accountID = rs.getInt("account_id");
+                        if (isSOTrx){
+                            accountID = rs.getInt("account_vta_id");
+                        }
+
+                        if (accountID > 0){
+                            linea.setC_ElementValue_ID(accountID);
+                        }
+
+                        if (linea.getC_Currency_ID() != as.getC_Currency_ID()){
+                            BigDecimal rate = CurrencyUtils.getCurrencyRateToAcctSchemaCurrency(getCtx(), this.getAD_Client_ID(), 0, linea.getC_Currency_ID(),
+                                    as.getC_Currency_ID(), 114, rs.getTimestamp("dateacct"), null);
+
+                            if (rate == null){
+
+                                MZGeneraFormDGIError dgiError = new MZGeneraFormDGIError(getCtx(), 0, get_TrxName());
+                                dgiError.setZ_GeneraFormDGI_ID(this.get_ID());
+                                dgiError.setErrorMsg("Falta tasa de Cambio para moneda : " + linea.getC_Currency_ID() + ", fecha : " + linea.getDateAcct().toString());
+                                dgiError.saveEx();
+                            }
+                            else{
+                                linea.setCurrencyRate(rate);
+                                linea.setAmtDocumentMT(linea.getAmtDocument().multiply(linea.getCurrencyRate()).setScale(2, RoundingMode.HALF_UP));
+                            }
+                        }
+
+                        MZAcctConfigRubroDGI configRubroDGI = MZAcctConfigRubroDGI.getByTax(getCtx(), linea.getC_Tax_ID(), isSOTrx, null);
+                        if ((configRubroDGI != null) && (configRubroDGI.get_ID() > 0)){
+                            linea.setZ_AcctConfigRubroDGI_ID(configRubroDGI.get_ID());
+                            linea.saveEx();
+                        }
+                        else{
+
+                            MTax tax = new MTax(getCtx(), rs.getInt("c_tax_id"), null);
+
+                            MZGeneraFormDGIError dgiError = new MZGeneraFormDGIError(getCtx(), 0, get_TrxName());
+                            dgiError.setZ_GeneraFormDGI_ID(this.get_ID());
+                            dgiError.setErrorMsg("Impuesto NO TIENE Rubro de DGI Asociado : " + tax.getName() + " - Documento : " + rs.getString("documentnoref"));
+                            dgiError.saveEx();
+                        }
+                    }
+                }
+
+            }
+
         }
         catch (Exception e){
             throw new AdempiereException(e);
